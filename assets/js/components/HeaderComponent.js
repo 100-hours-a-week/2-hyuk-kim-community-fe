ec3871dac14c5abb9b9a8c8623c1cde28b846b5b
sanghyuk Kim <tkdgur5031@naver.com>
2024-10-30 01:25:35 +0900

    feat: header-component 추가 custom tag로 추가 및 css 일부 수정

---
+++ assets/js/components/HeaderComponent.js
@@ -0,0 +1,25 @@
+class HeaderComponent extends HTMLElement {
+    constructor() {
+        super()
+        console.log("HeaderComponent connected!!");
+        this.attachShadow({mode: 'open'})
+    }
+
+    connectedCallback() {
+        this.shadowRoot.innerHTML = this.template()
+    }
+
+    template() {
+        return `
+        <link rel="stylesheet" type="text/css" href="/assets/css/header.css">
+        <header>
+            <div class="wrap">
+                <p id="logo">아무 말 대잔치</p>
+            </div>
+        </header>
+        `
+    }
+}
+
+window.customElements.define('header-component', HeaderComponent);
+// export default headerComponent;
