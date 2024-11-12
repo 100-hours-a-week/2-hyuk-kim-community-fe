ad2d48211357f46735115cf6af2af80e549d3c0b
sanghyuk Kim <tkdgur5031@naver.com>
2024-11-12 11:06:54 +0900

    fix: ApiFetch.js res.body가 null인 경우 오류 발생하는 문제 수정

--- assets/js/api/ApiFetch.js
+++ assets/js/api/ApiFetch.js
@@ -1,7 +1,3 @@
-// api.js (또는 원하는 파일명)
-// const constants = require('./constants');
-// import constants from './constants';
-
 export default async function apiFetch(endpoint, method, body = null) {
     const options = {
         method: method,
@@ -11,22 +7,13 @@ export default async function apiFetch(endpoint, method, body = null) {
         },
     };
 
-    if (body) {
-        options.body = JSON.stringify(body);
-    }
+    if (body) options.body = JSON.stringify(body);
 
-    // try {
     const response = await fetch(endpoint, options);
     if (!(response.status >= 200 && response.status < 300)) {
-        // 상태 코드를 포함한 에러 객체를 생성
         const error = new Error(`HTTP error! status: ${response.status}`);
         error.status = response.status; // 상태 코드 추가
         throw error;
     }
-    return await response.json();
-    // }
-    // catch (error) {
-    //     // console.error('Fetch error:', error);
-    //     throw error; // 오류를 호출하는 함수로 전달
-    // }
+    return await response.json() || "";
 }
